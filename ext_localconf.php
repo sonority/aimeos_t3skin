6633d8ee5f4af811569d0622143798ff24f69555
stephankellermayr <stephan.kellermayr@gmail.com>
2021-01-13 15:25:49 +0100

    So sad, fixed a typo...

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,4 +5,4 @@ if (!defined('TYPO3_MODE')) {
 }
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['aimeos']['extDirs']['1_aimeos_t3skin'] =
-  'EXT:aimeos_t3skin/Resources/Private/Extensions/';
+  'EXT:aimeos_t3skin/Resources/Private/Extensions/aimeos_t3skin/';
